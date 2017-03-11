5d2ddf8e42b0e8c3864cb1721ebf8548435cb2d0
Christoph Groß <meltiger@gmx.at>
2017-03-11 15:09:31 +0100

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("TODO: have fun!");
+		System.out.println("TODO: have fun! :)");
 		//TODO: don't have fun! work!
 	}
 
