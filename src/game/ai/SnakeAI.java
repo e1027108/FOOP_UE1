93259a081225d03f30d17777780bf6dbdbedc30e
Patrick <Patrick@Patrick-PC>
2017-03-23 13:34:40 +0100

    (patrick) created bare AI class

---
+++ src/game/ai/SnakeAI.java
@@ -0,0 +1,60 @@
+package game.ai;
+
+import game.Point;
+import game.Snake;
+
+public class SnakeAI implements Snake{
+
+	public SnakeAI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public char getDirection() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Point[] getBody() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void initPosition(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isAlive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setAlive(boolean alive) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Point getLastTailPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
