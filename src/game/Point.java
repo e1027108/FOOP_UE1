19fcccfbbefeb2cbff235352d01ffa2f7989c1d0
Christoph Gross <e1025119@student.tuwien.ac.at>
2017-04-11 18:06:42 +0200

    Point.equals() von eclipse erzeugen lassen

--- src/game/Point.java
+++ src/game/Point.java
@@ -30,13 +30,29 @@ public class Point {
 	public String toString() {
 		return "(" + this.x + "," + this.y + ")";
 	}
-	
-	public boolean equals(Point other){
-		if(this.x == other.getX() && this.y == other.getY()){
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
 			return true;
-		}
-		else{
+		if (obj == null)
 			return false;
-		}
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
 	}
 }
