6bc5ded651e12f626efef014d3881ce16b1e4143
Christoph Gross <e1025119@student.tuwien.ac.at>
2017-04-27 18:38:13 +0200

    server with first init (listening for players) phase

--- src/server/Server.java
+++ src/server/Server.java
@@ -4,29 +4,63 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
 
-	private static final int PORT = 1234;
-	private static final String HOST = "127.0.0.1";
+	private static final String CLIENT_THREAD = "AcceptClients";
+	private static Server server;
+	private ServerSocket serverSocket;
+	private List<Socket> clientSockets;
 
-	public static void main(String... args) throws IOException {
+	private static Thread acceptClients;
+
+	private Server() {
+	}
+
+	private Server(int port) throws IOException {
+		ServerSocket serverSocket = new ServerSocket();
+		clientSockets = new ArrayList<Socket>();
+		server = null;
+		acceptClients = new Thread(CLIENT_THREAD);
+		acceptClients.setDaemon(true);
+	}
+
+	public static Server getServer(int port) throws IOException {
+		if (server == null) {
+			server = new Server(port);
+		}
+		return server;
+	}
+
+	public static Server getServer() throws IOException {
+		if (server == null) {
+			return null;
+		}
+		return server;
+	}
+
+	public void init() throws IOException, SocketException {
+		acceptClients.start();
 		while (true) {
-			ServerSocket serverSocket = new ServerSocket(PORT);
-			System.out.println("Server: listening");
-			try {
+			if (Thread.currentThread().getName().equals(CLIENT_THREAD)) {
 				Socket clientSocket = serverSocket.accept();
-				System.out.println("Server: accepted");
-				try {
-					PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-					out.println("Hallo, test test :D");
-				} finally {
-					clientSocket.close();
-				}
-			} finally {
-				serverSocket.close();
+				clientSockets.add(clientSocket);
+				System.out.println("Server: accepted new client");
+			}
+			for (Socket cs : clientSockets) {
+				PrintWriter out = new PrintWriter(cs.getOutputStream(), true);
+				out.println("Hallo, test test :D");
 			}
 		}
+	}
+
+	// TODO more methods/threads for other server tasks
+
+	public void initDone() {
+		acceptClients.interrupt();
 	}
 
 }
