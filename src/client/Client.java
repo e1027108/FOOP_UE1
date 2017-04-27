d715b09515828c6a8f8a5abb12d61f0b6a406e8c
Christoph Gross <e1025119@student.tuwien.ac.at>
2017-04-27 15:32:54 +0200

    Server/Client skelett

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,22 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class Client {
 
+	private static final String HOST = "127.0.0.1";
+	private static final int PORT = 1234;
+	private Socket sock;
+
+	public static void main(String... args) throws IOException {
+		Socket sock = new Socket(HOST, PORT);
+		BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		String ret = in.readLine();
+		System.out.println("Client: " + ret);
+		in.close();
+		sock.close();
+	}
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,32 @@
 package server;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+	private static final int PORT = 1234;
+	private static final String HOST = "127.0.0.1";
+
+	public static void main(String... args) throws IOException {
+		while (true) {
+			ServerSocket serverSocket = new ServerSocket(PORT);
+			System.out.println("Server: listening");
+			try {
+				Socket clientSocket = serverSocket.accept();
+				System.out.println("Server: accepted");
+				try {
+					PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+					out.println("Hallo, test test :D");
+				} finally {
+					clientSocket.close();
+				}
+			} finally {
+				serverSocket.close();
+			}
+		}
+	}
 
 }
